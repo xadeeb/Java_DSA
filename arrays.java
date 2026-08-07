da6efb53b403606aa9a4088026f8948242ce8d14
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-07 09:09:53 +0530

    basic

---
+++ arrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the array:");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The elements of the array are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
