5e62808a4b1aecd866791a85f19ad821dc67fc3b
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-08 22:39:13 +0530

    pairs in the array

---
+++ pairsInarray.java
@@ -0,0 +1,18 @@
+public class pairsInarray {
+    
+
+    public static void main(String[] args) {
+        
+        int arr[] = {2,4,6,8,10};
+
+        for(int i=0;i<arr.length;i++){
+
+            //inner loop
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("("+arr[i]+" , " + arr[j] + ") ");
+            }
+            System.out.println();
+        }
+
+    }
+}
