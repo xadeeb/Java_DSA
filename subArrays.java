496e4ab2c7ea8e8e96ec9308449ac8fcab784bb7
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-11 12:24:54 +0530

    Number of Sub-arrays

--- subArrays.java
+++ subArrays.java
@@ -2,7 +2,11 @@ public class subArrays {
 
     public static void subArray(int arr[]){
 
+        int sum = 0;
+
         for(int i=0;i<arr.length;i++){
+
+            
 
             for(int j=i;j<arr.length;j++){
                 
@@ -10,13 +14,17 @@ public class subArrays {
                 for(int k=i;k<=j;k++){
 
                     System.out.print(arr[k] +  "  ");
-
+                    
                 }
+                sum++;
 
                 System.out.println("]");
             }
             System.out.println();
+
+            
         }
+        System.out.println("Total numbers of subArrays are " + sum);
     }
     
     public static void main(String[] args) {
