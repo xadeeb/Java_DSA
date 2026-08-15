a49f0169e6019f09a0d708af1b6903a89aa8399a
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-15 23:03:53 +0530

    insertion sort

---
+++ insertionSort.java
@@ -0,0 +1,43 @@
+public class insertionSort {
+    
+    
+    public static void insertion(int arr[]){
+
+       
+
+        for(int i=0;i<arr.length-1;i++){
+            
+            int curr = arr[i];
+            int prev = i-1;
+
+            while(prev >= 0 && arr[prev] > curr){
+
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+
+            arr[prev+1] = curr;
+        }
+
+    }
+
+    public static void print(int arr[]){
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = {24,56,34,25,12,78,2,1,78};
+
+        insertion(arr);
+
+        print(arr);
+
+        System.out.println();
+
+
+    }
+}
