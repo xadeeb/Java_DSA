28ee354fdf6650cb2bb3f6ed09d907f49fc458a5
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-15 23:23:48 +0530

    counting sort

---
+++ countingSort.java
@@ -0,0 +1,51 @@
+public class countingSort {
+    
+    public static void counting(int arr[]){
+
+       int largest = Integer.MIN_VALUE;
+
+       for(int i=0;i<arr.length;i++){
+            largest = Math.max(largest, arr[i]);
+        
+        }
+
+        int count[] = new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+
+        //sorting
+
+        int j = 0;
+        for(int i=0;i<count.length;i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+        
+
+    }
+
+    public static void print(int arr[]){
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = {24,56,34,25,12,78,2,1,78};
+
+        counting(arr);
+
+        print(arr);
+
+        System.out.println();
+
+
+    }
+}
