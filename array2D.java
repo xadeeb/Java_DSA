90d4e019141b3082c19b3a5edc64c3f8834ce6c2
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-17 23:54:12 +0530

    2d array file creation

---
+++ array2D.java
@@ -0,0 +1,7 @@
+public class array2D {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("tanu");
+    }
+}
--- countingSort.java
+++ countingSort.java
@@ -10,6 +10,7 @@ public class countingSort {
         }
 
         int count[] = new int[largest+1];
+        
         for(int i=0;i<arr.length;i++){
             count[arr[i]]++;
         }
