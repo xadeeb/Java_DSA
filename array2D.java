8a2626227c534dc68ffbb8787c9f8d55501f2423
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-19 23:10:33 +0530

    to green github graph

--- array2D.java
+++ array2D.java
@@ -2,6 +2,7 @@ public class array2D {
     
     public static void main(String[] args) {
         
-        System.out.println("tanu");
+        System.out.println("1708");
+        System.out.println("Too Green graph of github");
     }
 }
