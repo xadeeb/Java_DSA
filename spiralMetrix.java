3b9d38f61f138d4ada12dde3a992d60138524b8d
Mohd Adeeb <174165970+xadeeb@users.noreply.github.com>
2026-08-24 22:46:03 +0530

    creating spiral matrix file

---
+++ spiralMetrix.java
@@ -0,0 +1,11 @@
+public class spiralMetrix {
+    
+    public static void matrixSpiral(int arr[][]){
+
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Tanu");
+    }
+}
